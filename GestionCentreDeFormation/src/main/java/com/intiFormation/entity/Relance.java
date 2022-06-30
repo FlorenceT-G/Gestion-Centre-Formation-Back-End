ffa793be1a2c1c04405e1f13fe088300d93051d2
PaulG <p.gaillard@net.estia.fr>
2022-06-30 16:58:48 +0200

    Premieres annotations

--- GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Relance.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Relance.java
@@ -1,34 +1,28 @@
 package com.intiFormation.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Relance {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private Participant participant;
 	private Assistant assistant;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Participant getParticipant() {
-		return participant;
-	}
-	public void setParticipant(Participant participant) {
-		this.participant = participant;
-	}
-	public Assistant getAssistant() {
-		return assistant;
-	}
-	public void setAssistant(Assistant assistant) {
-		this.assistant = assistant;
-	}
-	
 	
 	public Relance() {
 		super();
 	}
 	
-	
+	public int getId() { return id; }
+	public Participant getParticipant() { return participant; }
+	public Assistant getAssistant() { return assistant; }
 
+	public void setId(int id) { this.id = id; }
+	public void setParticipant(Participant participant) { this.participant = participant; }
+	public void setAssistant(Assistant assistant) { this.assistant = assistant; }
+	
 }
--- GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Utilisateur.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Utilisateur.java
@@ -2,79 +2,62 @@ package com.intiFormation.entity;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Utilisateur {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	private String nom;
 	private String prenom;
 	private String adresseMail;
-	private String login;
+	private String username;
 	private String password;
-	private List<Role> roles;
+	
+	@OneToMany
+	@JoinColumn(name="idRole")
+	private Role role;
 
-	
-	
 	public Utilisateur() {
 		super();
 	}
 	
-	
-	
-	
-	public List<Role> getRoles() {
-		return roles;
+	public int getId() { return id; }
+	public String getNom() { return nom; }
+	public String getPrenom() { return prenom; }
+	public String getAdresseMail() { return adresseMail; }
+	public String getPassword() { return password; }
+
+	public void setId(int id) { this.id = id; }
+	public void setNom(String nom) { this.nom = nom; }
+	public void setPrenom(String prenom) { this.prenom = prenom; }
+	public void setAdresseMail(String adresseMail) { this.adresseMail = adresseMail; }
+	public void setPassword(String password) { this.password = password; }
+
+	public String getUsername() {
+		return username;
 	}
 
-
-
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
+	public Role getRole() {
+		return role;
+	}
 
-
-
-	public int getId() {
-		return id;
+	public void setRole(Role role) {
+		this.role = role;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public String getAdresseMail() {
-		return adresseMail;
-	}
-	public void setAdresseMail(String adresseMail) {
-		this.adresseMail = adresseMail;
-	}
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
 	
 }
