b4e6b5f723cb77554b05fe395f3fde28daf62d34
FlorenceT-G <florence.thomas.g@outlook.fr>
2022-06-30 17:08:59 +0200

    utilisateur et role

--- GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Role.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Role.java
@@ -1,15 +1,26 @@
 package com.intiFormation.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Role {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idRole;
 	private String libRole;
 	private String description;
 	
-	
+	@ManyToMany(mappedBy="role")
+	private List<Utilisateur> utilisateurs;
 	
 	
 	public int getIdRole() {
--- GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Utilisateur.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/entity/Utilisateur.java
@@ -1,15 +1,27 @@
 package com.intiFormation.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
 public class Utilisateur {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	private String nom;
 	private String prenom;
 	private String adresseMail;
-	private String login;
+	private String username;
 	private String password;
-
 	
+	@OneToMany
+	@JoinColumn(name="idRole")
+	private Role role;
 	
 	public Utilisateur() {
 		super();
