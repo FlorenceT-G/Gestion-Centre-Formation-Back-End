8911cbb9f6acf7915348e2a5b3408fa9b4872568
Riana MohamedYsuf <riana_ysuf@yahoo.fr>
2022-06-30 15:02:34 +0200

    ProjetAPI

---
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/GestionCentreDeFormationApplication.java
@@ -0,0 +1,13 @@
+package com.intiFormation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCentreDeFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCentreDeFormationApplication.class, args);
+	}
+
+}
