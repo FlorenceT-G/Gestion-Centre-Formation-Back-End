06898318faaa5312186039acfd8f9eb26652eea5
BFaustini <benjamin.faustini@hotmail.fr>
2022-07-01 14:06:22 +0200

    bfrestcont

--- GestionCentreDeFormation/src/main/java/com/intiFormation/controller/ParticipantController.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/controller/ParticipantController.java
@@ -2,16 +2,21 @@ package com.intiFormation.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.intiFormation.entity.Participant;
 import com.intiFormation.service.IParticipantService;
 
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ParticipantController {
 	
 	IParticipantService IpService;
--- GestionCentreDeFormation/src/main/java/com/intiFormation/controller/RelanceController.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/controller/RelanceController.java
@@ -3,17 +3,21 @@ package com.intiFormation.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.intiFormation.entity.Relance;
 import com.intiFormation.service.IRelanceService;
 
-@Controller
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class RelanceController {
 	
 	IRelanceService IrService;
