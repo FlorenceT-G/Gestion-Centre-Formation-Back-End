48151bb1c5a5a4fbe03246a7a2f1275ccf4d9740
FlorenceT-G <florence.thomas.g@outlook.fr>
2022-07-01 13:02:15 +0200

    important dans secuconfig et application.properties

--- GestionCentreDeFormation/src/main/java/com/intiFormation/securityConfig/SecurityConfiguration.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/securityConfig/SecurityConfiguration.java
@@ -1,9 +1,9 @@
 package com.intiFormation.securityConfig;
-import javax.ws.rs.HttpMethod;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
