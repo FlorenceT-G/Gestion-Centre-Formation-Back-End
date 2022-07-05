9d87bf4fc4da292d14945401e53f0a8c6450ad3e
PaulG <p.gaillard@net.estia.fr>
2022-07-05 14:29:50 +0200

    Amélioration méthode csvFile

--- GestionCentreDeFormation/src/main/java/com/intiFormation/controller/ProspectController.java
+++ GestionCentreDeFormation/src/main/java/com/intiFormation/controller/ProspectController.java
@@ -1,7 +1,10 @@
 package com.intiFormation.controller;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -63,14 +66,14 @@ public class ProspectController {
 		return (p);
 	}
 	
-	@GetMapping("/prospects/{num}")
+	@GetMapping("/prospect/{num}")
 	public Prospect selectnum(@PathVariable("num") long num)
 	{
 		Prospect p =  IpService.SelectByNum(num);
 		return (p);
 	}
 	
-	@GetMapping("/prospects/{mail}")
+	@GetMapping("/prospect/{mail}")
 	public Prospect selectmail(@PathVariable("mail") String mail)
 	{
 		Prospect p =  IpService.SelectByEmail(mail);
@@ -90,23 +93,31 @@ public class ProspectController {
 	}
 	
 	@GetMapping("/csvFile")	
-	public void csvReader(@RequestParam("file") MultipartFile file, HttpSession session) {
+	public void csvReader(@RequestParam("file") MultipartFile file,
+			@RequestParam("delimiter") String delimiter,
+			HttpSession session) {
+		
 		String filename = file.getOriginalFilename();
-		String path = "C:\\Users\\p.gaillard\\Downloads\\"+filename;
+		
+		String path = session.getServletContext().getRealPath("/")+filename;
+		System.out.println(path); // Chemin dans lequel va être créé le fichier .csv
 		
 		// List<Prospect> prospects = new ArrayList<>();
 		Scanner sc = null;
 		try {
+			byte TabBytes[]=file.getBytes();
+			
+			BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(path));
+			bout.write(TabBytes);
+			bout.flush();
+			bout.close();
 			sc = new Scanner(new File(path)); //parsing a CSV file into the constructor of Scanner class 
-
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		// Prtoposer à l'utilisateur d'utiliser son délimiteur
-		
-	    sc.useDelimiter(","); //setting comma as delimiter pattern
-
+				
+	    sc.useDelimiter(delimiter); // L'utilisateur doit choisir son délimiteur
+	    
 	    while (sc.hasNext()) {
 	    	// String Tampon = sc.next();
 	    	Prospect prospect = new Prospect();
